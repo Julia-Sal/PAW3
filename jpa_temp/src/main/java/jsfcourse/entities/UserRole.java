24e9f317fcd79a925f97fd2aa6b83fa8a6c0d467
Julia Sałacka <salacka.julia@gmail.com>
2022-11-14 14:49:59 +0100

    6. Database

---
+++ jpa_temp/src/main/java/jsfcourse/entities/UserRole.java
@@ -0,0 +1,58 @@
+package jsfcourse.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the user_role database table.
+ * 
+ */
+@Entity
+@Table(name="user_role")
+@NamedQuery(name="UserRole.findAll", query="SELECT u FROM UserRole u")
+public class UserRole implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idU_R;
+
+	//bi-directional many-to-one association to Role
+	@ManyToOne
+	@JoinColumn(name="idRole")
+	private Role role;
+
+	//bi-directional many-to-one association to User
+	@ManyToOne
+	@JoinColumn(name="idUser")
+	private User user;
+
+	public UserRole() {
+	}
+
+	public int getIdU_R() {
+		return this.idU_R;
+	}
+
+	public void setIdU_R(int idU_R) {
+		this.idU_R = idU_R;
+	}
+
+	public Role getRole() {
+		return this.role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
